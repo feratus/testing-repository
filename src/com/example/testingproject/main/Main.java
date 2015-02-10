0457f81c096b90f093395e4b6f85d52db5e76c06
feratus.gr <feratus.gr@gmail.com>
2015-02-09 22:07:11 -0600

    Second commit: delete build system files

--- src/com/example/testingproject/main/Main.java
+++ src/com/example/testingproject/main/Main.java
@@ -1,5 +1,15 @@
 package com.example.testingproject.main;
 
-public class Main {
+import com.example.testing_project.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
 	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+	}
 }
